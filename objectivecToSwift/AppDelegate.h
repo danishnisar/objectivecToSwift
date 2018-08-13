3379d8760a6a67f71d750480e5253983b4f93cf4
Danish Nisar <danishnisar.aptechnn@gmail.com>
2018-08-13 18:17:11 +0500

    Initial Commit

---
+++ objectivecToSwift/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  objectivecToSwift
+//
+//  Created by MacBook Prp on 13/08/2018.
+//  Copyright © 2018 Native Brains. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ objectivecToSwift/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  objectivecToSwift
+//
+//  Created by MacBook Prp on 13/08/2018.
+//  Copyright © 2018 Native Brains. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
